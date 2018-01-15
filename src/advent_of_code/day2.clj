cc97b45012649be53520afbb43a6be9892eb337b
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2018-01-16 09:27:01 +1000

    Day 2, part 1

---
+++ src/advent_of_code/day2.clj
@@ -0,0 +1,25 @@
+(ns advent-of-code.day2
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def input (io/resource "day2.txt"))
+
+(def read-input
+  (-> (slurp input)
+      (str/trim)
+      (str/split #"\n")))
+
+(defn part1 []
+  (reduce
+    (fn [acc e]
+      (let [[l h w] (map #(Integer/parseInt %) (str/split e #"x"))]
+        (+
+         acc
+         (* 2 l w )
+         (* 2 w h)
+         (* 2 h l)
+         (apply * (take 2 (sort [l h w]))))))
+    0
+    read-input))
+
+(part1)
