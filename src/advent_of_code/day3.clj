fa43d4fb76522e3d59e534eca326c4ce3b15d669
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2018-01-20 00:10:03 +1000

    Day 3, part 2

--- src/advent_of_code/day3.clj
+++ src/advent_of_code/day3.clj
@@ -16,13 +16,20 @@
     \^ [[:y] inc]
     \v [[:y] dec]))
 
-(defn part1 []
-  (loop [input  read-input
+(defn go [directions]
+  (loop [input  directions
          coords {:x 0 :y 0}
          result []]
     (if (empty? input)
-      (count (distinct result))
+      result
       (let [direction (get-direction (first input))]
         (recur (next input)
                (apply update-in coords direction)
                (conj result (vals coords)))))))
+
+(defn part1 []
+  (count (distinct (go read-input))))
+
+(defn part2 []
+  (count (distinct (into (go (take-nth 2 read-input))
+        (go (take-nth 2 (next read-input)))))))
